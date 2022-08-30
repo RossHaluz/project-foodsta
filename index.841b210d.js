b31b351555850f83dced81b4438a62c0185b205a
RossHaluz <106879174+RossHaluz@users.noreply.github.com>
2022-08-30 13:43:43 +0000

    Deploying to gh-pages from @ RossHaluz/project-foodsta@6b3e56aa119deb4bcfc0f4b0a8a77d5f2b44d574 🚀

--- index.841b210d.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,navigation:{nextEl:".swiper__btn--next",prevEl:".swiper__btn--prev"}});
-//# sourceMappingURL=index.841b210d.js.map
--- index.c7c03c14.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,navigation:{nextEl:".swiper__btn--next",prevEl:".swiper__btn--prev"}});
-//# sourceMappingURL=index.c7c03c14.js.map
